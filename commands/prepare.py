135eea89d01025f83324909dba7376d695c2231e
Cris Barbero <cfbarbero@gmail.com>
2020-05-11 11:55:09 -0600

    Fix default on node_data parameter

--- commands/prepare.py
+++ commands/prepare.py
@@ -802,7 +802,7 @@ def run(arguments):
     parser.set_defaults(read_replicas=True)
     parser.set_defaults(azs=True)
     parser.set_defaults(collapse_asgs=True)
-    parser.set_defaults(node_data=False)
+    parser.set_defaults(node_data=True)
     parser.set_defaults(default_route=True)
     args = parser.parse_args(arguments)
 
